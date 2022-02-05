7758cbb60a9c367393f097e0a6166a69ce8be7e0
Ste_ph_en <stephen.nehrbass@gmail.com>
2022-02-05 14:23:15 -0500

    clean up bin

--- bin/lock.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-xset s 500 &
-xautolock -time 5 -locker "betterlockscreen -l" -notify 30 -notifier "notify-send 'Locker' 'Locking screen in 30 seconds'" -killtime 5 -killer "systemctl suspend"
