5525629902aa8aa61dc492a9b78158461d0c6071
Ste_ph_en <stephen.nehrbass@gmail.com>
2022-04-25 16:18:25 -0400

    minor style changes

--- .config/nwg-wrapper/date-wttr.sh
+++ .config/nwg-wrapper/date-wttr.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 time=$(LC_ALL=C date +'%A, %d. %B')
-wttr=$(curl https://wttr.in/?format=1)
-echo '<span size="45000" foreground="#a9a1e1">'$time'</span><span size="35000" foreground="#9ca0a4">'
-echo $wttr'</span>'
+echo '<span size="45000" foreground="#a9a1e1">'$time'</span><span size="35000" foreground="#9ca0a4"></span>'
