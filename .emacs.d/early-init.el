bcc2ee6a63cc5b175648825c9e9755dad5321fbe
nehrbash <stephen.nehrbass@infinitetactics.com>
2022-08-30 23:15:47 -0400

    load path env & proper enable of delete-selection-mode

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,21 +1,21 @@
 (setq gc-cons-threshold (* 50 1000 1000))
+(setq inhibit-startup-screen t)
 (setq inhibit-startup-message t)
-(setq initial-scratch-message nil)
 (setq inhibit-startup-message t)
 (setq inhibit-startup-echo-area-message t)
+(setq initial-scratch-message nil)
+(add-to-list 'default-frame-alist '(font . "Source Code Pro 12"))
 (set-face-attribute 'default nil
                     :family "Source Code Pro"
-                    :height 110
+                    :height 120
                     :weight 'normal
                     :width 'normal)
-(add-to-list 'default-frame-alist
-             '(background-color . "#282c34")) ;; doom one
-;;(add-to-list 'default-frame-alist '(font .  "Source Code Pro Medium 14"))
+(set-fontset-font "fontset-default" '(#x5d0 . #x5ff) "FontAwesome")
 (tool-bar-mode -1)
 (set-scroll-bar-mode nil)
 (menu-bar-mode -1)
 
 (setq package-enable-at-startup nil)
-
+(pixel-scroll-precision-mode t)
 ;; So we can detect this having been loaded
 (provide 'early-init)
