b8f78478497edde37d56408347e5b998699ac9ef
nehrbash <stephen.nehrbass@infinitetactics.com>
2023-05-08 00:32:24 -0400

    add enabling my services

--- install.sh
+++ install.sh
@@ -11,6 +11,15 @@ paru --needed -S - < pkglist.txt
 mkdir -p "$HOME/.zsh"
 git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+sudo touch /etc/systemd/system/getty@tty1.service.d/override.conf
+
+systemctl --user enable hyprland.service
+systemctl --user enable hyprpaper.service
+systemctl --user enable hyprshell.service
+systemctl --user enable eww.service
+# systemctl --user enable swaync.service
+
 spicetify config current_theme Onepunch color_scheme light
 spicetify apply
 
