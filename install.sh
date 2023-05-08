f9929c21ec21232fa8cd59244fc618fccb15b0f7
nehrbash <stephen.nehrbass@infinitetactics.com>
2023-05-07 23:18:41 -0400

    readme

--- install.sh
+++ install.sh
@@ -8,6 +8,12 @@ rm -r paru
 
 paru --needed -S - < pkglist.txt
 
+mkdir -p "$HOME/.zsh"
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+
+spicetify config current_theme Onepunch color_scheme light
+spicetify apply
+
 xdg-mime default emacsclient.desktop application/pdf
 xdg-mime default emacsclient.desktop inode/directory
 
