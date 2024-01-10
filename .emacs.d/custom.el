ff447856039fc3dd79cb9433df1b01aa6cc635f8
nehrbash <stephen.nehrbass@infinitetactics.com>
2024-01-09 23:19:59 -0500

    stuff

---
+++ .emacs.d/custom.el
@@ -0,0 +1,47 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(codeium/metadata/api_key "63f7cf48-c3b2-47e4-bd09-1851f67b38d8")
+ '(org-fold-catch-invisible-edits 'show-and-error nil nil "Customized with use-package org")
+ '(package-selected-packages
+   '(codeium corfu-candidate-overlay corfu-terminal cus-dir gcmh indent-bars org-appear org-margin org-roam-ui
+			 pdf-continuous-scroll-mode protogg whisper)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((((class color) (min-colors 256)) :font "Source Code Pro" :height 115)))
+ '(blamer-face ((((class color) (min-colors 256)) :foreground "#a2c080" :italic t)))
+ '(fringe ((t :background "#352718")))
+ '(header-line ((t :box (:line-width 4 :color "#483426" :style nil))))
+ '(header-line-highlight ((t :box (:color "#e8e4b1"))))
+ '(keycast-key ((t)))
+ '(line-number ((t :background "#352718")))
+ '(mode-line ((t :box (:line-width 6 :color "#704f00" :style nil))))
+ '(mode-line-active ((t :box (:line-width 6 :color "#704f00" :style nil))))
+ '(mode-line-highlight ((t :box (:color "#e8e4b1"))))
+ '(mode-line-inactive ((t :box (:line-width 6 :color "#59463f" :style nil))))
+ '(org-document-title ((((class color) (min-colors 256)) :height 1.8)))
+ '(org-modern-done ((((class color) (min-colors 256)) :height 1.2)))
+ '(org-modern-tag ((((class color) (min-colors 256)) :height 1.2)))
+ '(org-modern-todo ((((class color) (min-colors 256)) :height 1.2)))
+ '(scroll-bar ((((class color) (min-colors 256)) :foreground "#59463f" :background "#483426")))
+ '(tab-bar-tab ((t :box (:line-width 4 :color "#352718" :style nil))))
+ '(tab-bar-tab-inactive ((t :box (:line-width 4 :color "#79665f" :style nil))))
+ '(tab-line ((((class color) (min-colors 256)) :foreground "#281d12" :background "#281d12" :box (:line-width 3 :color "#483426"))))
+ '(tab-line-env-1 ((((class color) (min-colors 256)) :background "#e89a88")))
+ '(tab-line-env-2 ((((class color) (min-colors 256)) :background "#eac29f")))
+ '(tab-line-env-3 ((((class color) (min-colors 256)) :background "#6ab4ef")))
+ '(tab-line-env-default ((((class color) (min-colors 256)) :background "#a0d26f")))
+ '(tab-line-highlight ((((class color) (min-colors 256)) :background "#79665f" :foreground "#281d12")))
+ '(tab-line-tab ((((class color) (min-colors 256)) :inherit 'tab-line :background "#a2c080" :foreground "#281d12")))
+ '(tab-line-tab-current ((((class color) (min-colors 256)) :background "#a2c080" :foreground "#281d12")))
+ '(tab-line-tab-inactive ((((class color) (min-colors 256)) :background "#90918a" :foreground "#281d12")))
+ '(term ((t :background "#281d12")))
+ '(unspecified-bg ((((class color) (min-colors 256)) :foreground "#352718" :background "#352718")))
+ '(window-divider ((t :background "#352718" :foreground "#352718")))
+ '(window-divider-first-pixel ((t :background "#352718" :foreground "#352718")))
+ '(window-divider-last-pixel ((t :background "#352718" :foreground "#352718"))))
