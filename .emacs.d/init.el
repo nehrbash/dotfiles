926aa25e59e8c516270f7977a100bf36d8bd0ce4
nehrbash <stephen.nehrbass@infinitetactics.com>
2024-01-12 11:20:35 -0500

    minor fixes to using monitor model name instead of id

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1791,7 +1791,7 @@ point reaches the beginning or end of the buffer, stop there."
 											  :unusedvariable t)
 									:hints
 									(:assignVariableTypes t
-														  :constantValues t
+:constantValues t
 														  :rangeVariableTypes t))
 				  :js-ts
                   (:format 
@@ -1827,6 +1827,38 @@ point reaches the beginning or end of the buffer, stop there."
  :vc (:url "https://github.com/jdtsmith/eglot-booster.git"
 			:branch "main" :rev :newest)
 	:config	(eglot-booster-mode))
+
+(use-package dape
+  :bind
+  (:map ("<F7>" . dape))
+  ;; To use window configuration like gud (gdb-mi)
+  :init
+  (setq dape-buffer-window-arrangement 'gud)
+
+  :config
+  ;; Info buffers to the right
+  ;; (setq dape-buffer-window-arrangement 'right)
+
+  ;; To not display info and/or buffers on startup
+  ;; (remove-hook 'dape-on-start-hooks 'dape-info)
+  (remove-hook 'dape-on-start-hooks 'dape-repl)
+
+  ;; To display info and/or repl buffers on stopped
+  ;; (add-hook 'dape-on-stopped-hooks 'dape-info)
+  ;; (add-hook 'dape-on-stopped-hooks 'dape-repl)
+
+  ;; By default dape uses gdb keybinding prefix
+  ;; If you do not want to use any prefix, set it to nil.
+  ;; (setq dape-key-prefix "\C-x\C-a")
+
+  ;; Kill compile buffer on build success
+  (add-hook 'dape-compile-compile-hooks 'kill-buffer)
+
+  ;; Save buffers on startup, useful for interpreted languages
+  (add-hook 'dape-on-start-hooks
+            (defun dape--save-on-start ()
+              (save-some-buffers t t)))
+  )
 
 (use-package git-gutter
   :defer t
